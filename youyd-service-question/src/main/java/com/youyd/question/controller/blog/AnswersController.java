6c12777c118ee95098b14a3924feba3f9e5ceff5
GuoGuang <guoguang0536@gmail.com>
2019-02-28 14:48:02 +0800

    :hammer: Modify layout

---
+++ youyd-service-question/src/main/java/com/youyd/question/controller/blog/AnswersController.java
@@ -0,0 +1,87 @@
+package com.youyd.question.controller.blog;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.youyd.pojo.Result;
+import com.youyd.question.pojo.Answers;
+import com.youyd.question.service.AnswersService;
+import com.youyd.utils.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @description: 问题答案
+ * @author: LGG
+ * @create: 2019-02-26
+ **/
+
+@RestController
+@RequestMapping("/answers")
+public class AnswersController {
+
+	private final AnswersService answersService;
+
+	@Autowired
+	public AnswersController(AnswersService answersService) {
+		this.answersService = answersService;
+	}
+
+
+	/**
+	 * 查询全部数据
+	 *
+	 * @return Result
+	 */
+	@GetMapping
+	public Result findAnswersByCondition(Answers answers) {
+		IPage<Answers> byCondition = answersService.findAnswersByCondition(answers);
+		return new Result(true, StatusCode.OK.getCode(), StatusCode.OK.getMsg(), byCondition);
+	}
+
+	/**
+	 * 根据ID查询
+	 *
+	 * @param id ID
+	 * @return Result
+	 */
+	@GetMapping(value = "/{id}")
+	public Result findAnswersByPrimaryKey(@PathVariable String id) {
+		Answers result = answersService.findAnswersByPrimaryKey(id);
+		return new Result(true, StatusCode.OK.getCode(), StatusCode.OK.getMsg(), result);
+	}
+
+
+	/**
+	 * 增加
+	 *
+	 * @param answers 要添加的对象
+	 */
+	@PostMapping
+	public Result insertAnswers(Answers answers) {
+		answersService.insertAnswers(answers);
+		return new Result(true, StatusCode.OK.getCode(), StatusCode.OK.getMsg());
+	}
+
+	/**
+	 * 修改
+	 * @param answers 被修改的对象
+	 */
+	@PutMapping()
+	public Result updateByAnswersSelective(Answers answers) {
+		boolean result = answersService.updateByAnswersSelective(answers);
+		return new Result(result, StatusCode.OK.getCode(), StatusCode.OK.getMsg());
+	}
+
+	/**
+	 * 删除
+	 *
+	 * @param answersIds 答案id数组
+	 */
+	@DeleteMapping()
+	public Result deleteByPrimaryKey(@RequestBody List answersIds) {
+		answersService.deleteByPrimaryKey(answersIds);
+		return new Result(true, StatusCode.OK.getCode(), StatusCode.OK.getMsg());
+	}
+
+}
