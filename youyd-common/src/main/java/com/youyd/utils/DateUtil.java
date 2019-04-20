5bbd3bbe83209765a2c860f00dc168b8ad963551
GuoGuang <guoguang0536@gmail.com>
2019-04-20 23:44:21 +0800

    :tada: Add date helper

---
+++ youyd-common/src/main/java/com/youyd/utils/DateUtil.java
@@ -0,0 +1,25 @@
+package com.youyd.utils;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+/**
+ * 日期工具类
+ * @author LGG
+ * @create 2019年4月20日22:35:18
+ * @version 1.0.0
+ */
+public class DateUtil {
+
+	/**
+	 * 获取时间戳，时区加8
+	 * @return ： Long
+	 */
+	public static Long getTimestamp(){
+		return LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli());
+	}
+}
