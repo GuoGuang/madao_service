579fceccfa640c11563d5770f808643f305a92d4
GuoGuang0536 <GuoGuang0536@gmail.com>
2019-04-24 00:50:37 +0800

    :construction: 用户枚举状态陆续添加中...

--- youyd-common/src/main/java/com/youyd/enumclass/UserEnum.java
+++ youyd-common/src/main/java/com/youyd/enumclass/UserEnum.java
@@ -1,15 +1,22 @@
 package com.youyd.enumclass;
 
 
+import lombok.Getter;
+
 /**
  * <p>用户模块 状态相关枚举</p>
  * @author LGG
  * @create 2019年4月23日22:35:18
  * @version 1.0.0
  */
+@Getter
 public enum UserEnum {
 
-	WRONG_PASSWORD(10001,"密码错误，请重试！");
+	WRONG_PASSWORD(10001,"登录名或登录密码不正确！"),
+	NICKNAME_REPEAT(10002,"该昵称已被他人使用！"),
+	ERROR_CODE(10003,"验证码错误！"),
+	FAILED_TOOMANY(10004,"登录失败次数太多！");
+
 
 	private Integer code;
 
@@ -20,12 +27,5 @@ public enum UserEnum {
 		this.info = info;
 	}
 
-	public Integer getCode() {
-		return code;
-	}
-
-	public String getInfo() {
-		return info;
-	}
 
 }
