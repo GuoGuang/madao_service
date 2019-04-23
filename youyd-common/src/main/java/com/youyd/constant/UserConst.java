02706fb2a8c0f4fcd75790b96300959e09222848
GuoGuang0536 <GuoGuang0536@gmail.com>
2019-04-23 23:59:28 +0800

    :art: 添加常量与枚举类

---
+++ youyd-common/src/main/java/com/youyd/constant/UserConst.java
@@ -0,0 +1,18 @@
+package com.youyd.constant;
+
+
+/**
+ * <p>用户模块 业务相关常量</p>
+ * @author LGG
+ * @create 2019年4月23日22:35:18
+ * @version 1.0.0
+ */
+public final class UserConst {
+
+	/* 常量禁止实例化 */
+	private UserConst() {
+		throw new IllegalStateException("Constant prohibition instantiation!");
+	}
+
+	private static final String UA= "";
+}
