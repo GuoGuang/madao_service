110d317c1e2846e497152556c241cb235a6d99b2
GuoGuang <guoguang0536@gmail.com>
2019-05-04 16:59:14 +0800

    :tada: 添加Feign接口及Hystrix断路器

---
+++ youyd-service-api/src/main/java/com/youyd/api/base/OptLogServiceRpc.java
@@ -0,0 +1,27 @@
+package com.youyd.api.base;
+
+import com.youyd.api.base.fallback.OptLogServiceRpcFallbackFactory;
+import com.youyd.constant.FeignConst;
+import com.youyd.pojo.base.OptLog;
+import com.youyd.utils.JsonData;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * 操作日志api
+ * @author : LGG
+ * @create : 2018-09-27
+ **/
+
+@FeignClient(value = FeignConst.SERVICE_BASE,path = "optLog",fallbackFactory = OptLogServiceRpcFallbackFactory.class)
+public interface OptLogServiceRpc {
+
+	/**
+	 * 增加操作日志
+	 * @param optLog 操作日志实体
+	 * @return JsonData
+	 */
+	@PostMapping
+	JsonData insertOptLog(@RequestBody OptLog optLog);
+}
