b3693e051f7971acfc26dbbe0e4cb9a7108c39b4
GuoGuang <guoguang0536@gmail.com>
2019-05-05 22:15:07 +0800

    :fire: 添加 任务调度配置

---
+++ youyd-common/src/main/java/com/youyd/utils/ScheduleJobFactory.java
@@ -0,0 +1,25 @@
+package com.youyd.utils;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.scheduling.quartz.AdaptableJobFactory;
+import org.springframework.stereotype.Component;
+
+  
+/** 定时任务管理类 
+ * @date 2016-4-9
+ */
+@Component
+public class ScheduleJobFactory extends AdaptableJobFactory {
+
+	@Autowired
+	private AutowireCapableBeanFactory autowireCapableBeanFactory;
+
+	@Override
+	protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+		Object jobInstance = super.createJobInstance(bundle);
+		autowireCapableBeanFactory.autowireBean(jobInstance);
+		return jobInstance;
+	}
+}
