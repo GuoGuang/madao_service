be33cda1c3091f5d7465b3771e3c4ed59a2047c3
GuoGuang0536 <GuoGuang0536@gmail.com>
2019-05-08 21:51:46 +0800

    :tada: 添加字段管理模块

---
+++ youyd-service-base/src/main/java/com/youyd/base/dao/DictDao.java
@@ -0,0 +1,14 @@
+package com.youyd.base.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.youyd.pojo.base.Dict;
+
+/**
+ * @description: 字典管理
+ * @author: LGG
+ * @create: 2019-05-08
+ **/
+
+public interface DictDao extends BaseMapper<Dict> {
+
+}
