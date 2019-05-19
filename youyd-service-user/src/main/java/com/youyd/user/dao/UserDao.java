def63918585bb60f3c17d709d576b44d3ad72e52
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-05-19 13:07:27 +0800

    :fire:

--- youyd-service-user/src/main/java/com/youyd/user/dao/UserDao.java
+++ youyd-service-user/src/main/java/com/youyd/user/dao/UserDao.java
@@ -5,9 +5,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.youyd.pojo.user.User;
 
 /**
- * @description: 用户管理
- * @author: LGG
- * @create: 2018-09-26 16:21
+ * 用户管理
+ * @author : LGG
+ * @create : 2018-09-26 16:21
  **/
 
 public interface UserDao extends BaseMapper<User> {
