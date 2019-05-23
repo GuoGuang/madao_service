9100ddbe04b8f0394eb4a6e5117d63e4c87e9aec
GuoGuang0536 <GuoGuang0536@gmail.com>
2019-05-23 14:40:59 +0800

    :tada: 添加吐槽模块

---
+++ youyd-common/src/main/java/com/youyd/pojo/tweets/TweetsComment.java
@@ -0,0 +1,63 @@
+package com.youyd.pojo.tweets;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.youyd.pojo.BasePojo;
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * 吐槽，评论表
+ * @author: LGG
+ * @create: 2019-05-23
+ **/
+@ApiModel(value="TweetsComment", description="吐槽，评论表")
+@Getter
+@Setter
+public class TweetsComment extends BasePojo implements Serializable {
+
+    /**
+     * 吐槽评论ID
+     */
+    @TableId(type = IdType.ID_WORKER_STR)
+    private String id;
+
+    /**
+     * 评论内容
+     */
+    private String content;
+
+    /**
+     * 评论人ID
+     */
+    private String userId;
+
+    /**
+     * 评论人昵称
+     */
+    private String nickName;
+
+    /**
+     * 点赞数
+     */
+    private Long thumbUpCount;
+
+    /**
+     * 是否可见
+     */
+    private Integer isVisible;
+
+    /**
+     * 吐槽表id
+     */
+    private String tweetsId;
+
+    /**
+     * 父级评论
+     */
+    private String parentId;
+
+}
