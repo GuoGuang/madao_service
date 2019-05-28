6681d173fb50ba5122c49116da5ba6caea0029c0
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-05-28 23:58:36 +0800

    :tada: 添加时间秒数常量

--- youyd-common/src/main/java/com/youyd/constant/CommonConst.java
+++ youyd-common/src/main/java/com/youyd/constant/CommonConst.java
@@ -15,6 +15,14 @@ public final class CommonConst {
 	}
 
 	public static final int ADD = 1;
-	public static final Integer DELETE= 2;
-	public static final Integer MODIFY= 3;
+	public static final int DELETE= 2;
+	public static final int MODIFY= 3;
+
+	/*超时时间*/
+	// 三个月
+	public static final Integer TIME_OUT_THREE_MONTHS = 60 * 60 * 24 * 30 * 3;
+	// 一周
+	public static final Integer TIME_OUT_WEEK = 60 * 60 * 24 * 7;
+	// 一天
+	public static final Integer TIME_OUT_DAY = 60 * 60 * 24;
 }
