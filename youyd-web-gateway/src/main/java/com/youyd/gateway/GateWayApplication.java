d5f7b47104de4a1937209ec4778a689695288661
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-06-10 19:16:10 +0800

    :tada: 添加WebFlux Security配置

--- youyd-web-gateway/src/main/java/com/youyd/gateway/GateWayApplication.java
+++ youyd-web-gateway/src/main/java/com/youyd/gateway/GateWayApplication.java
@@ -2,21 +2,27 @@ package com.youyd.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 /**
- * @description: gateway服务网关
- * @author: LGG
- * @create: 2018-09-26 14:34
+ * gateway服务网关
+ * @author : LGG
+ * @create : 2018-09-26 14:34
  **/
 
 @ComponentScan(basePackages = {"com.youyd"})
-@SpringBootApplication()
+@SpringBootApplication
 public class GateWayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GateWayApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
