9def8e48fead85460ecd03eb636329ee4b2f9c05
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-06-18 20:17:56 +0800

    :tada: 子服务关闭Security注解

--- youyd-service-user/src/main/java/com/youyd/user/UserApplication.java
+++ youyd-service-user/src/main/java/com/youyd/user/UserApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -30,7 +29,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableEurekaClient
 @EnableSwagger2
 @EnableFeignClients(basePackages = "com.youyd.api")
-@EnableWebSecurity
+//@EnableWebSecurity
 @EnableAdminServer
 @ComponentScan(basePackages = {"com.youyd"})
 public class UserApplication {
