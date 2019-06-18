9fa3e9d079db9fd3fb0c065d169d897ef2813b47
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-06-18 20:33:23 +0800

    :zap: 更新Jackson配置

--- youyd-common/src/main/java/com/youyd/config/JacksonConfig.java
+++ youyd-common/src/main/java/com/youyd/config/JacksonConfig.java
@@ -1,11 +1,15 @@
 package com.youyd.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import java.io.IOException;
 
 /**
  * https://blog.csdn.net/kobejayandy/article/details/45869861
@@ -17,12 +21,32 @@ public class JacksonConfig {
 	@Primary
 	@ConditionalOnMissingBean(ObjectMapper.class)
 	public ObjectMapper jacksonObjectMapper(){
-		ObjectMapper mapper = new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
 		// （禁止SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS）
 		// 解决 No serializer found for class org.hibernate.proxy.pojo.javassist.JavassistL
-		mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
 
-		return mapper;
+		// 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
+		// Include.Include.ALWAYS 默认
+		// Include.NON_DEFAULT 属性为默认值不序列化
+		// Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的。这样对移动端会更省流量
+		// Include.NON_NULL 属性为NULL 不序列化
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		// 允许出现特殊字符和转义符
+		objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+		// 允许出现单引号
+		objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+		// 字段保留，将null值转为""
+		objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>()
+		{
+			@Override
+			public void serialize(Object o, JsonGenerator jsonGenerator,SerializerProvider serializerProvider)throws IOException
+			{
+				jsonGenerator.writeString("");
+			}
+		});
+		return objectMapper;
 	}
 
 }
