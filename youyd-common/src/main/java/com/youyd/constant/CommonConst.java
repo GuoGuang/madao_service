48a1eee20360704e6f44649dc89286079dfa9f82
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-06-23 01:21:40 +0800

    :wrench: 扩展常量类

--- youyd-common/src/main/java/com/youyd/constant/CommonConst.java
+++ youyd-common/src/main/java/com/youyd/constant/CommonConst.java
@@ -7,22 +7,32 @@ package com.youyd.constant;
  * @create 2019年4月23日22:35:18
  * @version 1.0.0
  */
-public final class CommonConst {
+public interface CommonConst {
 
-	/* 常量禁止实例化 */
-	private CommonConst() {
-		throw new IllegalStateException("Constant prohibition instantiation!");
-	}
-
-	public static final int ADD = 1;
-	public static final int DELETE= 2;
-	public static final int MODIFY= 3;
+	int ADD = 1;
+	int DELETE= 2;
+	int MODIFY= 3;
 
 	/*超时时间*/
+	// 半小时
+	Integer TIME_OUT_HALF_HOUR = 60 * 30;
 	// 三个月
-	public static final Integer TIME_OUT_THREE_MONTHS = 60 * 60 * 24 * 30 * 3;
+	Integer TIME_OUT_THREE_MONTHS = 60 * 60 * 24 * 30 * 3;
 	// 一周
-	public static final Integer TIME_OUT_WEEK = 60 * 60 * 24 * 7;
+	Integer TIME_OUT_WEEK = 60 * 60 * 24 * 7;
 	// 一天
-	public static final Integer TIME_OUT_DAY = 60 * 60 * 24;
+	Integer TIME_OUT_DAY = 60 * 60 * 24;
+
+	/**
+	 * 验证图片验证码时，http请求中默认的携带图片验证码信息的参数的名称
+	 */
+	String DEFAULT_PARAMETER_NAME_CODE_IMAGE = "captcha";
+	/**
+	 * 验证短信验证码时，http请求中默认的携带短信验证码信息的参数的名称
+	 */
+	String DEFAULT_PARAMETER_NAME_CODE_SMS = "smsCode";
+	/**
+	 * 发送短信验证码 或 验证短信验证码时，传递手机号的参数的名称
+	 */
+	String DEFAULT_PARAMETER_NAME_MOBILE = "mobile";
 }
