bfb53dc3f6286d434ea7b03f436aee0e33d7b44d
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-08-09 22:17:44 +0800

    :wrench: Update JacksonConfig

--- youyd-common/src/main/java/com/youyd/config/JacksonConfig.java
+++ youyd-common/src/main/java/com/youyd/config/JacksonConfig.java
@@ -32,7 +32,7 @@ public class JacksonConfig {
 		// Include.NON_DEFAULT 属性为默认值不序列化
 		// Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的。这样对移动端会更省流量
 		// Include.NON_NULL 属性为NULL 不序列化
-		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		// 允许出现特殊字符和转义符
 		objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
