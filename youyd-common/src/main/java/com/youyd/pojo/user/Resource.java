064ac2c081652b4bc466bf87233ec31623cf636e
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-09-10 21:30:32 +0800

    :ok_hand: rename menu

--- youyd-common/src/main/java/com/youyd/pojo/user/Resource.java
+++ youyd-common/src/main/java/com/youyd/pojo/user/Resource.java
@@ -1,16 +1,67 @@
 package com.youyd.pojo.user;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.youyd.pojo.BasePojo;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@NoArgsConstructor
-public class Resource extends BasePojo {
-    private String code = "12";
-    private String name;
-    private String type;
-    private String url;
-    private String method;
-    private String description;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *  资源实体
+ * @author LGG
+ * @create 2018-09-27
+ **/
+@Getter
+@Setter
+public class Resource extends BasePojo implements Serializable,Cloneable {
+
+	@TableId(type = IdType.ID_WORKER_STR)
+	private String id; // 资源表主键
+
+	@NotNull(message="名称不能为空")
+	@Pattern(regexp="([a-zA-Z0-9\u4E00-\u9FA5]{2,10})",message="必须是2到10位(字母，数字)名称！")
+	private String name; // 名称
+
+	private String parentId; // 父资源id
+
+	private String component; // vue组件名称/名称必须真实存在
+
+	private String icon; // 图标
+
+	private String path; // 路径
+
+	private float sort; // 排序
+
+	private Integer isHidden; // 是否隐藏
+
+	@NotNull(message="描述不能为空")
+	private String description; // 描述
+
+	private String type; // btn 或resource
+	private String url; // 请求url，跟path不同，path为vue的组件请求路径，url是网络请求路径
+	private String method;  // 请求方法
+	private String code;  // 资源标识
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Resource resource = (Resource) o;
+		return id.equals(resource.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
 }
