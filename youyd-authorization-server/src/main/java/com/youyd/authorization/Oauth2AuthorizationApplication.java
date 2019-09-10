0ac1bf1d0ba89980c6e901ea0cd2788a27076a0f
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-09-10 21:35:10 +0800

    :beers: support okhttp

--- youyd-authorization-server/src/main/java/com/youyd/authorization/Oauth2AuthorizationApplication.java
+++ youyd-authorization-server/src/main/java/com/youyd/authorization/Oauth2AuthorizationApplication.java
@@ -1,8 +1,14 @@
 package com.youyd.authorization;
 
+import com.youyd.utils.IdGenerate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 
 /**
@@ -11,9 +17,25 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @create : 2019-06-13
  **/
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.youyd.api")
+@ComponentScan(basePackages = {"com.youyd"})
 @EnableDiscoveryClient
 public class Oauth2AuthorizationApplication {
     public static void main(String[] args) {
         SpringApplication.run(Oauth2AuthorizationApplication.class, args);
     }
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+	}
+	/**
+	 * 雪花算法 id生成器
+	 * @return IdGenerate
+	 */
+	@Bean
+	public IdGenerate idGenerate(){
+		return new IdGenerate(1,1);
+	}
+
+
 }
