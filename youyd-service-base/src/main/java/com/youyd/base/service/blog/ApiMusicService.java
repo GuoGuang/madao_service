932be6f4dfa2fee29c34173ed15a6e7dd2f528a9
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-09-18 01:51:12 +0800

    :tada: Initial commit.

---
+++ youyd-service-base/src/main/java/com/youyd/base/service/blog/ApiMusicService.java
@@ -0,0 +1,45 @@
+package com.youyd.base.service.blog;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.youyd.base.dao.MusicDao;
+import com.youyd.pojo.base.Music;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 音乐
+ * @author LGG
+ * @create 2019-09-15
+ **/
+@Service
+public class ApiMusicService {
+
+	private final MusicDao musicDao;
+
+	@Autowired
+	public ApiMusicService(MusicDao musicDao) {
+		this.musicDao = musicDao;
+	}
+
+	/**
+	 * 查询所有音乐
+	 * @return List
+	 */
+	public List<Music> findMusicByCondition() {
+		LambdaQueryWrapper<Music> queryWrapper = new LambdaQueryWrapper<>();
+		List<Music> musics = musicDao.selectList(queryWrapper);
+		return musics;
+	}
+
+	/**
+	 * 音乐详情
+	 * @return List
+	 */
+	public Music findMusicDetail(String resId) {
+		return musicDao.selectById(resId);
+	}
+
+}
