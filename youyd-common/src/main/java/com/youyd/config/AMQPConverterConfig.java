485ba257637536d9956c40cc856586b7d7319ad6
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-09-19 17:11:19 +0800

    :tada: Initial commit.

---
+++ youyd-common/src/main/java/com/youyd/config/AMQPConverterConfig.java
@@ -0,0 +1,21 @@
+package com.youyd.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自定义rabbitMQ序列化转换器
+ * @author  GuoGuang
+ * @create  2019-09-19 15:38
+ **/
+
+@Configuration
+public class AMQPConverterConfig {
+
+    @Bean
+    public MessageConverter messageConverter (){
+        return  new Jackson2JsonMessageConverter();
+    }
+}
