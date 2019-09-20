1d5b87e32dfce92357a589d087e5a5eb4a5f3464
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-09-21 00:24:24 +0800

    :heavy_plus_sign: Adding @EnableRabbit

--- youyd-authentication-server/src/main/java/com/youyd/auth/Oauth2AuthenticationApplication.java
+++ youyd-authentication-server/src/main/java/com/youyd/auth/Oauth2AuthenticationApplication.java
@@ -1,6 +1,7 @@
 package com.youyd.auth;
 
 import com.youyd.properties.SecurityProperties;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -21,6 +22,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients(basePackages = "com.youyd.api")
 @ComponentScan(basePackages = {"com.youyd"})
 @EnableDiscoveryClient
+@EnableRabbit // 启用RabbitMQ
 public class Oauth2AuthenticationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2AuthenticationApplication.class, args);
