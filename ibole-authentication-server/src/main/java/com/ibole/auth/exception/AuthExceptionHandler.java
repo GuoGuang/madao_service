0bc3b906962ae745afd51497692564b2422f4849
guoguang <guoguang0536@gmail.com>
2019-11-20 22:24:20 +0800

    :package: Updating compiled files or packages.

---
+++ ibole-authentication-server/src/main/java/com/ibole/auth/exception/AuthExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.ibole.auth.exception;
+
+import com.ibole.enums.StatusEnum;
+import com.ibole.utils.JsonData;
+import com.ibole.utils.LogBack;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 统一系统异常处理类
+ **/
+@RestControllerAdvice
+public class AuthExceptionHandler {
+
+    /**
+     * ValidateCodeException
+     *
+     * @param ex ValidateCodeException
+     */
+    @ExceptionHandler(ValidateCodeException.class)
+    public JsonData validateCodeException(ValidateCodeException ex) {
+        LogBack.error(ex.getMessage(), ex);
+        return new JsonData(StatusEnum.PARAM_ILLEGAL);
+    }
+
+}
