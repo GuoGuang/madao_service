5aee76b7185ceecfec9af9ffc0363c204db39b3d
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-06-13 23:04:37 +0800

    :white_check_mark: optimization tests.

--- codeway-service-article/src/test/java/com/codeway/article/controller/backstage/ArticleControllerTest.java
+++ codeway-service-article/src/test/java/com/codeway/article/controller/backstage/ArticleControllerTest.java
@@ -1,110 +1,124 @@
 package com.codeway.article.controller.backstage;
 
+import com.codeway.article.filter.CustomFilter;
 import com.codeway.article.service.backstage.ArticleService;
 import com.codeway.pojo.article.Article;
 import com.codeway.utils.JsonData;
-import org.junit.Before;
+import com.codeway.utils.JsonUtil;
+import com.codeway.utils.OssClientUtil;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ArticleControllerTest {
-    @Mock
-    ArticleService articleService;
+	@Mock
+	ArticleService articleService;
+	@Mock
+	OssClientUtil ossClientUtil;
+	@Mock
+	Pageable pageable;
+	@InjectMocks
+	ArticleController articleController;
 
-    @InjectMocks
-    ArticleController articleController;
+	private MockMvc mockMvc;
 
-    private MockMvc mockMvc;
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders
+				.standaloneSetup(articleController)
+				.setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver()) // 解决Pageable参数问题
+				.addFilter(CustomFilter::doFilter)
+				.build();
+	}
 
-    @Before
-    public void init(){
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(articleController)
-                .build();
-    }
+	@Test
+	void testFindArticleByCondition() throws Exception {
 
-    @Test
-    public void testFindArticleByCondition() throws Exception {
+		Article article = new Article();
+		article.setUserName("foo");
+		article.setId("1");
 
-        List<Article> users = Arrays.asList();
+		Page<Article> page = new PageImpl<>(Arrays.asList(article));
 
-        when(articleService.findArticleByCondition(any(), any())).thenReturn(null);
+		when(articleService.findArticleByCondition(any(), any())).thenReturn(page);
 
-        mockMvc.perform(get("/article"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].id", is(1)))
-                .andExpect(jsonPath("$[0].username", is("Foo")))
-                .andExpect(jsonPath("$[1].id", is(2)))
-                .andExpect(jsonPath("$[1].username", is("Bar")));
+		ResultActions resultActions = mockMvc.perform(get("/article").accept(MediaType.APPLICATION_JSON));
+		resultActions.andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.data.content", hasSize(1)));
 
-        verify(articleService, times(1)).findArticleByCondition(any(), any());
-        verifyNoMoreInteractions(articleService);
+		MockHttpServletResponse response = resultActions.andReturn().getResponse();
+		String attributeNames = response.getContentAsString();
+		JsonData jsonData = JsonUtil.jsonToPojo(attributeNames, JsonData.class);
+		System.out.println(jsonData);
 
+		verify(articleService, times(1)).findArticleByCondition(any(), any());
+		verifyNoMoreInteractions(articleService);
 
-        JsonData<Page<Article>> result = articleController.findArticleByCondition(new Article(), null);
-        Assertions.assertEquals(new JsonData<Page<Article>>(true, 0, "message", any()), result);
-    }
+	}
 
-    @Test
-    void testFindArticleById() {
-        when(articleService.findArticleById(anyString())).thenReturn(new Article());
+	@Test
+	void testFindArticleById() {
+		when(articleService.findArticleById(anyString())).thenReturn(new Article());
 
-        JsonData<Article> result = articleController.findArticleById("id");
-        Assertions.assertEquals(new JsonData<Article>(true, 0, "message", any()), result);
-    }
+		JsonData<Article> result = articleController.findArticleById("id");
+		Assertions.assertEquals(new JsonData<Article>(true, 0, "message", any()), result);
+	}
 
-    @Test
-    void testInsertArticle() {
-        JsonData<Map<String, String>> result = articleController.insertArticle(new Article(), null);
-        Assertions.assertEquals(new JsonData<Map<String, String>>(true, 0, "message", any()), result);
-    }
+	@Test
+	void testInsertArticle() {
+		JsonData<Map<String, String>> result = articleController.insertArticle(new Article(), null);
+		Assertions.assertEquals(new JsonData<Map<String, String>>(true, 0, "message", any()), result);
+	}
 
-    @Test
-    void testUpdateByPrimaryKeySelective() {
-        JsonData<Void> result = articleController.updateByPrimaryKeySelective(new Article(), null);
-        Assertions.assertEquals(new JsonData<Void>(true, 0, "message", any()), result);
-    }
+	@Test
+	void testUpdateByPrimaryKeySelective() {
+		JsonData<Void> result = articleController.updateByPrimaryKeySelective(new Article(), null);
+		Assertions.assertEquals(new JsonData<Void>(true, 0, "message", any()), result);
+	}
 
-    @Test
-    void testDeleteArticleByIds() {
-        JsonData<Void> result = articleController.deleteArticleByIds(Arrays.<String>asList("String"));
-        Assertions.assertEquals(new JsonData<Void>(true, 0, "message", any()), result);
-    }
+	@Test
+	void testDeleteArticleByIds() {
+		JsonData<Void> result = articleController.deleteArticleByIds(Arrays.<String>asList("String"));
+		Assertions.assertEquals(new JsonData<Void>(true, 0, "message", any()), result);
+	}
 
-    @Test
-    void testExamine() {
-        JsonData<Void> result = articleController.examine("id");
-        Assertions.assertEquals(new JsonData<Void>(true, 0, "message", any()), result);
-    }
+	@Test
+	void testExamine() {
+		JsonData<Void> result = articleController.examine("id");
+		Assertions.assertEquals(new JsonData<Void>(true, 0, "message", any()), result);
+	}
 
-    @Test
-    void testGetUserInfo() {
-        Map<String, String> result = articleController.getUserInfo(null);
-        Assertions.assertEquals(new HashMap<String, String>() {{
-            put("String", "String");
-        }}, result);
-    }
+	@Test
+	void testGetUserInfo() {
+		Map<String, String> result = articleController.getUserInfo(null);
+		Assertions.assertEquals(new HashMap<String, String>() {{
+			put("String", "String");
+		}}, result);
+	}
 }
 
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
