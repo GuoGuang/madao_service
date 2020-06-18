d153e75cfbe6da5fa9a072b49029bb96d3991701
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-06-18 22:01:21 +0800

    :bento: Adding domain

--- codeway-web-gateway/src/main/java/com/codeway/gateway/config/CorsConfig.java
+++ codeway-web-gateway/src/main/java/com/codeway/gateway/config/CorsConfig.java
@@ -27,6 +27,8 @@ public class CorsConfig {
 		corsConfiguration.addAllowedOrigin("http://202.182.114.223:3000");
 		corsConfiguration.addAllowedOrigin("http://codeway.me");
 		corsConfiguration.addAllowedOrigin("https://codeway.me");
+		corsConfiguration.addAllowedOrigin("http://codeway.fun");
+		corsConfiguration.addAllowedOrigin("https://codeway.fun");
 		corsConfiguration.addAllowedOrigin("http://www.codeway.me");
 		corsConfiguration.addAllowedOrigin("https://www.codeway.me");
 		corsConfiguration.addAllowedHeader("*");
