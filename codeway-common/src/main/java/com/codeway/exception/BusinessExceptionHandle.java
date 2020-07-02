f68436521cb4ed974674c1dbfece2483fdcbf231
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-07-02 20:40:34 +0800

    :ok_hand: fix priority issues

--- codeway-common/src/main/java/com/codeway/exception/BusinessExceptionHandle.java
+++ codeway-common/src/main/java/com/codeway/exception/BusinessExceptionHandle.java
@@ -4,6 +4,8 @@ import com.codeway.exception.custom.ResourceNotFoundException;
 import com.codeway.exception.custom.UserException;
 import com.codeway.utils.JsonData;
 import com.codeway.utils.LogBack;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * 统一业务处理类
  **/
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class BusinessExceptionHandle {
 	/**
 	 * 用户相关异常
