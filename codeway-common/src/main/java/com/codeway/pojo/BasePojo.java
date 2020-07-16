aae78d91f2003c4144c426cf4e31f77f8c094987
guoguang <guoguang0536@gmail.com>
2020-07-16 17:51:35 +0800

    :art: Improving structure

--- codeway-common/src/main/java/com/codeway/pojo/BasePojo.java
+++ codeway-common/src/main/java/com/codeway/pojo/BasePojo.java
@@ -1,7 +1,6 @@
 package com.codeway.pojo;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,8 +10,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-@Getter
-@Setter
+@Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BasePojo implements Serializable {
