2f98f7227ee40607af2970f75ff9bc228332b799
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-08-04 23:52:27 +0800

    :memo: Writing docs.

--- codeway-common/src/main/java/com/codeway/annotation/OptLog.java
+++ codeway-common/src/main/java/com/codeway/annotation/OptLog.java
@@ -7,13 +7,20 @@ import java.lang.annotation.*;
  * 操作日志切面
  **/
 
-@Target({ElementType.METHOD,ElementType.TYPE})
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface OptLog {
 
-	/* 要执行的操作类型比如：add操作 */
+	/**
+	 * 要执行的操作类型比如：add操作
+	 * {@link com.codeway.constant.CommonConst}
+	 */
 	int operationType() default 0;
-	/* 要执行的具体操作比如：添加用户 */
+
+	/**
+	 * 要执行的具体操作比如：添加用户
+	 * {@link com.codeway.constant.CommonConst}
+	 */
 	String operationName() default "";
 }
