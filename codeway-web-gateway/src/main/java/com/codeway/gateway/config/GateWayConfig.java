dc35a1420e5933407860029e23cb5ced999a81bd
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-08-17 22:28:51 +0800

    :white_check_mark: Adding tests part

--- codeway-web-gateway/src/main/java/com/codeway/gateway/config/GateWayConfig.java
+++ codeway-web-gateway/src/main/java/com/codeway/gateway/config/GateWayConfig.java
@@ -17,39 +17,44 @@ import org.springframework.context.annotation.Configuration;
  * 易于编写谓词和过滤器
  * 请求率限制
  * 路径重写
+ *
+ * .and().after 可以实现接口在xx时间后放开，但是网关层尽量保证单纯不要掺杂业务逻辑在其中；
  **/
 
 @Configuration
 public class GateWayConfig {
 
 	/**
-	 * 自定义拦截规则
-	 *     id：固定，不同 id 对应不同的功能，可参考 官方文档
-	 *     uri：目标服务地址
-	 *     predicates：路由条件
-	 *     filters：过滤规则
-	 *     stripPrefix(0)：剥夺前缀数字为几则删除路径上的几位，stripPrefix(2) /name/bar/foo -》 http://nameservice/foo.
-	 *     lb = load-balanced（负载均衡）
+	 * custom blocking rules
+	 * id：固定，不同 id 对应不同的功能，可参考 官方文档
+	 * uri：target service address
+	 * predicates：routing conditions
+	 * filters：filter rule
+	 * stripPrefix(0)：剥夺前缀数字为几则删除路径上的几位，stripPrefix(2) /name/bar/foo -》 http://nameservice/foo.
+	 * lb = load-balanced（负载均衡）
+	 *
 	 * @param builder
 	 * @return RouteLocator
 	 */
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
-				// 用户服务
+				// user
 				.route("user_route", r -> r.path("/su/**").filters(f -> f.stripPrefix(1)).uri("lb://SERVICE-USER"))
-				// 基础服务
+				// base
 				.route("base_route", r -> r.path("/ba/**").filters(f -> f.stripPrefix(1)).uri("lb://SERVICE-BASE"))
-				// 文章服务
+				// article
 				.route("article_route", a -> a.path("/ar/**").filters(f -> f.stripPrefix(1)).uri("lb://SERVICE-ARTICLE"))
 
-				// 授权、鉴权、第三方登录
+				// authorization, authentication, third-party login
 				.route("auth_route", a -> a.path("/oauth/**").filters(f -> f.stripPrefix(0)).uri("lb://AUTHENTICATION-SERVER"))
 				.route("social_route", a -> a.path("/social/**").filters(f -> f.stripPrefix(1)).uri("lb://AUTHENTICATION-SERVER"))
 
 				// API
 				.route("api_base_route", r -> r.path("/api/ba/**").filters(f -> f.stripPrefix(0)).uri("lb://SERVICE-BASE"))
-				.route("api_article_route", a -> a.path("/api/ar/**").filters(f -> f.stripPrefix(0)).uri("lb://SERVICE-ARTICLE"))
+				.route("api_article_route", a -> a.path("/api/ar/**")
+//						.and().after(ZonedDateTime.now().plusMinutes(10)) // execute in ten minutes
+						.filters(f -> f.stripPrefix(0)).uri("lb://SERVICE-ARTICLE"))
 				.route("api_user_route", a -> a.path("/api/su/**").filters(f -> f.stripPrefix(0)).uri("lb://SERVICE-USER"))
 
 				// python
