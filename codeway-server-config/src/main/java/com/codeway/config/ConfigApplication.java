2c3c19cd93c1b87a93866b17f9ac94061b00b83b
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-08-24 23:13:23 +0800

    :art: Improving code doc

---
+++ codeway-server-config/src/main/java/com/codeway/config/ConfigApplication.java
@@ -0,0 +1,42 @@
+package com.codeway.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+	@Configuration
+	@EnableWebSecurity
+	public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		/**
+		 * actuator、key禁止访问
+		 */
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			http.authorizeRequests()
+					.antMatchers("/actuator/**").authenticated()
+					.antMatchers("/key/**").authenticated()
+					.antMatchers("/decrypt").authenticated()
+					.anyRequest().permitAll();
+		}
+	  /*  @Override
+	    public void configure(WebSecurity web){
+	        web.ignoring().antMatchers("/actuator/**");
+	    }*/
+	}
+}
--- codeway-server-eureka/src/main/java/com/codeway/eureka/EurekaApplication.java
+++ codeway-server-eureka/src/main/java/com/codeway/eureka/EurekaApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * eureka
- **/
-
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
