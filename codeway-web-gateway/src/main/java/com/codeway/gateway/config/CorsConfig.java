2c34c372fa742c37235c1b3546a9d8fcd10a8a0c
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-10-26 23:22:23 +0800

    :wrench: 修复安全问题

--- codeway-web-gateway/src/main/java/com/codeway/gateway/config/CorsConfig.java
+++ codeway-web-gateway/src/main/java/com/codeway/gateway/config/CorsConfig.java
@@ -1,28 +1,37 @@
 package com.codeway.gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.util.pattern.PathPatternParser;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
 
+	@Autowired
+	Environment environment;
+
 	@Bean
-	public CorsWebFilter corsFilter(){
+	public CorsWebFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
 		source.registerCorsConfiguration("/**", buildConfig());
 		return new CorsWebFilter(source);
 	}
 
-	private CorsConfiguration buildConfig(){
+	private CorsConfiguration buildConfig() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		//在生产环境上最好指定域名，以免产生跨域安全问题
+		String[] activeProfiles = environment.getActiveProfiles();
+		if (Arrays.asList(activeProfiles).contains("dev")) {
+			corsConfiguration.addAllowedOrigin("http://localhost:9527");
+			corsConfiguration.addAllowedOrigin("http://127.0.0.1:3000");
+		}
 		corsConfiguration.addAllowedOrigin("http://admin.codeway.fun");
-		// 前台博客
-		corsConfiguration.addAllowedOrigin("http://127.0.0.1:3000");
 		corsConfiguration.addAllowedOrigin("http://codeway.me");
 		corsConfiguration.addAllowedOrigin("https://codeway.me");
 		corsConfiguration.addAllowedOrigin("http://codeway.fun");
