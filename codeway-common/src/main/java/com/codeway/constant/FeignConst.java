0e9f426593e10a80d89e9835d73597da8f1b8cad
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-10-26 23:22:41 +0800

    :wrench: 添加接口常量

--- codeway-common/src/main/java/com/codeway/constant/FeignConst.java
+++ codeway-common/src/main/java/com/codeway/constant/FeignConst.java
@@ -7,17 +7,18 @@ public final class FeignConst {
 	}
 
 	/* 基础微服务 */
-	public static final String SERVICE_BASE  = "service-base";
-	public static final String SERVICE_BASE_LOGIN_LOG_PATH  = "/loginLog";
-	public static final String SERVICE_BASE_OPT_LOG_PATH  = "/optLog";
+	public static final String SERVICE_BASE = "service-base";
+	public static final String SERVICE_BASE_LOGIN_LOG_PATH = "/loginLog";
+	public static final String SERVICE_BASE_OPT_LOG_PATH = "/optLog";
 
 	/* 用户微服务 */
-	public static final String SERVICE_USER  = "service-user";
-	public static final String SERVICE_USER_PATH  = "/user";
-	public static final String SERVICE_RESOURCE_PATH  = "/resource";
+	public static final String SERVICE_USER = "service-user";
+	public static final String SERVICE_USER_PATH = "/user";
+	public static final String SERVICE_BLOG_USER_PATH = "/api/su";
+	public static final String SERVICE_RESOURCE_PATH = "/resource";
 
 	/* 认证微服务&授权微服务 */
-	public static final String SERVICE_AUTHENTICATION_AUTH  = "authentication-server";
-	public static final String SERVICE_AUTHENTICATION_AUTH_PATH  = "/";
+	public static final String SERVICE_AUTHENTICATION_AUTH = "authentication-server";
+	public static final String SERVICE_AUTHENTICATION_AUTH_PATH = "/";
 
 }
