c75e5b23c3809e8f670c6de849891fec7a1fba86
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-10-31 14:11:12 +0800

    :art: 支持Feign调用传输header

--- codeway-service-api/src/main/java/com/codeway/FeignConfiguration.java
+++ codeway-service-api/src/main/java/com/codeway/FeignConfiguration.java
@@ -5,15 +5,21 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.apache.http.HttpHeaders;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.*;
 
 /**
  * 扩展 Feign  get传输pojo
+ * Feign调用携带请求头
+ *
  * @see https://stackoverflow.com/questions/53546840/spring-cloud-openfeign-failed-to-create-dynamic-query-parameters
  **/
 @Configuration
@@ -37,6 +43,28 @@ public class FeignConfiguration implements RequestInterceptor {
 				LogBack.error("IOException occurred while try to create http query", e);
 			}
 		}
+
+		// Feign调用携带请求头
+		ServletRequestAttributes attributes = (ServletRequestAttributes)
+				RequestContextHolder.getRequestAttributes();
+		if (attributes != null) {
+			HttpServletRequest request = attributes.getRequest();
+			Enumeration<String> headerNames = request.getHeaderNames();
+			if (headerNames != null) {
+				while (headerNames.hasMoreElements()) {
+					String name = headerNames.nextElement();
+					// 只转发Authorization头
+					// https://blog.csdn.net/qq_39986681/article/details/107138740
+					if (HttpHeaders.AUTHORIZATION.equalsIgnoreCase(name)) {
+						String values = request.getHeader(name);
+						template.header(name, values);
+					}
+
+				}
+			}
+		}
+
+
 	}
 
 	private void buildQuery(JsonNode jsonNode, String path, Map<String, Collection<String>> queries) {
