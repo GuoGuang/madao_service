c471d4e656b489047d11ebeeb6dbd8c7a5537e96
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-11-06 01:37:13 +0800

    :alien: 添加扩展字段

--- codeway-authentication-server/src/main/java/com/codeway/auth/config/CustomUserAuthenticationConverter.java
+++ codeway-authentication-server/src/main/java/com/codeway/auth/config/CustomUserAuthenticationConverter.java
@@ -1,10 +1,8 @@
 package com.codeway.auth.config;
 
 import com.codeway.auth.service.UserJwt;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
 import org.springframework.stereotype.Component;
@@ -31,21 +29,25 @@ public class CustomUserAuthenticationConverter extends DefaultUserAuthentication
 	 */
 	@Override
 	public Map<String, ?> convertUserAuthentication(Authentication authentication) {
-		LinkedHashMap<String,Object> response = new LinkedHashMap<>();
+		LinkedHashMap<String, Object> response = new LinkedHashMap<>();
 		String name = authentication.getName();
 		Object principal = authentication.getPrincipal();
 		UserJwt userJwt = null;
-		if(principal instanceof  UserJwt){
+		/*if(principal instanceof  UserJwt){
 			userJwt = (UserJwt) principal;
 		}else{
 			//refresh_token默认不去调用userdetailService获取用户信息，手动去调用，得到 UserJwt
 			UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(name);
 			userJwt = (UserJwt) userDetails;
-		}
+		}*/
 		if (authentication.getAuthorities() != null && !authentication.getAuthorities().isEmpty()) {
 			response.put("authorities", AuthorityUtils.authorityListToSet(authentication.getAuthorities()));
 		}
-		response.put("id", userJwt.getId());
+//		response.put("id", userJwt.getId());
+		response.put("id", authentication.getName());
+		// 需配置user_name，否则SecurityContextHolder.getContext().getAuthentication().getName
+		// 拿不到值，DefaultUserAuthenticationConverter#extractAuthentication()
+		response.put("user_name", name);
 		return response;
 	}
 
