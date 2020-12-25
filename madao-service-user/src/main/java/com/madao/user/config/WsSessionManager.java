9aef4d4e9130b49e56377b1b87d3a8adb29043d3
guoguang <guoguang0536@gmail.com>
2020-12-25 17:23:48 +0800

    :sparkles: 引入websocket功能

---
+++ madao-service-user/src/main/java/com/madao/user/config/WsSessionManager.java
@@ -0,0 +1,65 @@
+package com.madao.user.config;
+
+import com.madao.utils.LogBack;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class WsSessionManager {
+
+    /**
+     * 保存连接 session
+     */
+    private static ConcurrentHashMap<String, WebSocketSession> SESSION_POOL = new ConcurrentHashMap<>();
+
+    /**
+     * 添加 session
+     *
+     * @param key
+     */
+    public static void add(String key, WebSocketSession session) {
+        // 添加 session
+        SESSION_POOL.put(key, session);
+    }
+
+    /**
+     * 删除 session,会返回删除的 session
+     *
+     * @param key
+     * @return
+     */
+    public static WebSocketSession remove(String key) {
+        // 删除 session
+        return SESSION_POOL.remove(key);
+    }
+
+    /**
+     * 删除并同步关闭连接
+     *
+     * @param key
+     */
+    public static void removeAndClose(String key) {
+        WebSocketSession session = remove(key);
+        if (session != null) {
+            try {
+                // 关闭连接
+                session.close();
+            } catch (IOException e) {
+                LogBack.error("删除并同步关闭连接时异常：{}",e.getMessage(),e);
+            }
+        }
+    }
+
+    /**
+     * 获得 session
+     *
+     * @param key
+     * @return
+     */
+    public static WebSocketSession get(String key) {
+        // 获得 session
+        return SESSION_POOL.get(key);
+    }
+}
