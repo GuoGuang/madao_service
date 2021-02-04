f9e4d11ef6eab2bdaa16e5c7a05b8adedc7481e7
guoguang <guoguang0536@gmail.com>
2021-02-04 16:34:30 +0800

    :wrench: 删除无效的@Profile注解

--- madao-service-user/src/main/java/com/madao/user/config/Swagger2Config.java
+++ madao-service-user/src/main/java/com/madao/user/config/Swagger2Config.java
@@ -9,10 +9,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@Profile({"dev"})
 public class Swagger2Config {
 
     @Bean
--- madao-web-gateway/src/main/java/com/madao/gateway/config/SwaggerConfig.java
+++ madao-web-gateway/src/main/java/com/madao/gateway/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.madao.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,10 +9,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@Profile({"dev"})
 public class SwaggerConfig {
 
     @Bean
