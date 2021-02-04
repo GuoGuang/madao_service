121b5b5868aa0cc04850bdafae9e8158b9d41d93
guoguang <guoguang0536@gmail.com>
2021-02-04 10:25:11 +0800

    :zap: Swagger3版本去除@EnableSwagger2注解

--- madao-web-gateway/src/main/java/com/madao/gateway/config/SwaggerConfig.java
+++ madao-web-gateway/src/main/java/com/madao/gateway/config/SwaggerConfig.java
@@ -13,7 +13,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 @Profile({"dev"})
 public class SwaggerConfig {
 
