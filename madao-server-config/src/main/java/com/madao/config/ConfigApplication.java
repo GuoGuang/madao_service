7954ad85b38a6a3c0136076380bd6279d867defc
guoguang0536@gmail.com <guoguang0536@gmail.com>
2021-02-12 10:08:09 +0800

    :recycle: 合并Eureka和Config

--- madao-server-config/src/main/java/com/madao/config/ConfigApplication.java
+++ madao-server-config/src/main/java/com/madao/config/ConfigApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
@@ -18,25 +16,5 @@ public class ConfigApplication {
         SpringApplication.run(ConfigApplication.class, args);
     }
 
-    @Configuration
-    @EnableWebSecurity
-    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-        /**
-         * actuator、key禁止访问
-         */
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-            http.authorizeRequests()
-                    .antMatchers("/actuator/**").authenticated()
-                    .antMatchers("/key/**").authenticated()
-                    .antMatchers("/decrypt").authenticated()
-                    .anyRequest().permitAll();
-        }
-	  /*  @Override
-	    public void configure(WebSecurity web){
-	        web.ignoring().antMatchers("/actuator/**");
-	    }*/
-    }
 }
---
+++ madao-server-config/src/main/java/com/madao/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.madao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	/**
+	 * actuator、key禁止访问
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+				.antMatchers("/actuator/**").authenticated()
+				.antMatchers("/key/**").authenticated()
+				.antMatchers("/decrypt").authenticated()
+				.anyRequest().permitAll();
+	}
+	  /*  @Override
+	    public void configure(WebSecurity web){
+	        web.ignoring().antMatchers("/actuator/**");
+	    }*/
+}
