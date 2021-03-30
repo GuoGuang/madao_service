417ac3bed1528f63bbc184ac093ab90696abf106
guoguang0536@gmail.com <guoguang0536@gmail.com>
2021-03-30 21:51:54 +0800

    :sparkles: 新增RabbitMQ延时队列功能

---
+++ madao-service-user/src/main/java/com/madao/user/config/RabbitMQConfig.java
@@ -0,0 +1,77 @@
+package com.madao.user.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 延时队列配置
+ */
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        return factory;
+    }
+
+    public static final String DELAY_EXCHANGE = "Ex.DelayExchange";
+    public static final String ORDER_USER_QUEUE    = "MQ.UserDelayQueue";
+    public static final String PRODUCT_ARTICLE_QUEUE    = "MQ.ArticleDelayQueue";
+    public static final String DELAY_USER_KEY      = "delay.user";
+    public static final String DELAY_ARTICLE_KEY      = "delay.article";
+
+    /**
+     * 延时交换机
+     * @return TopicExchange
+     */
+    @Bean
+    public TopicExchange delayExchange() {
+        Map<String, Object> pros = new HashMap<>(3);
+        //设置交换机支持延迟消息推送
+        pros.put("x-delayed-message", "topic");
+        TopicExchange exchange = new TopicExchange(DELAY_EXCHANGE, true, false, pros);
+        //我们在也可以在 Exchange 的声明中可以设置exchange.setDelayed(true)来开启延迟队列
+        exchange.setDelayed(true);
+        return exchange;
+    }
+
+    /**
+     * 延时队列
+     * @return Queue
+     */
+    @Bean("userDelayQueue")
+    public Queue userDelayQueue() {
+        return new Queue(ORDER_USER_QUEUE, true);
+    }
+    @Bean("articleDelayQueue")
+    public Queue articleDelayQueue() {
+        return new Queue(PRODUCT_ARTICLE_QUEUE, true);
+    }
+
+    /**
+     * 绑定队列和交换机,以及设定路由规则key
+     *
+     * @return Binding
+     */
+    @Bean
+    public Binding delayOrderBinding() {
+        return BindingBuilder.bind(userDelayQueue()).to(delayExchange()).with(DELAY_USER_KEY);
+    }
+    @Bean
+    public Binding delayArticleBinding() {
+        return BindingBuilder.bind(articleDelayQueue()).to(delayExchange()).with(DELAY_ARTICLE_KEY);
+    }
+}
---
+++ madao-service-user/src/main/java/com/madao/user/mq/MQSender.java
@@ -0,0 +1,58 @@
+package com.madao.user.mq;
+
+import com.madao.user.config.RabbitMQConfig;
+import com.madao.utils.LogBack;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * MQ发送器
+ */
+@Component
+public class MQSender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+	public MQSender(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
+    final RabbitTemplate.ConfirmCallback confirmCallback = (correlationData, ack, cause) -> {
+	    LogBack.error("correlationData:{}",correlationData);
+	    LogBack.error("ack:{}",ack);
+        if (!ack) {
+        	LogBack.error("异常处理...");
+        }
+    };
+
+    final RabbitTemplate.ReturnCallback returnCallback = (message, replyCode, replyText, exchange, routingKey) ->
+		    LogBack.info("return exchange:{},routingKey:{}, replyCode:{}, replyText: {} ",exchange,routingKey,replyCode,replyText);
+
+	/**
+	 * 发送延时消息
+	 * @param message 消息
+	 * @param delayTime 延时时间：毫秒
+	 */
+	public void sendDelay(Object message, int delayTime) {
+        //采用消息确认模式，消息发出去后，异步等待响应
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.setReturnCallback(returnCallback);
+        //id + 时间戳 全局唯一
+        CorrelationData correlationData = new CorrelationData("delay" + System.nanoTime());
+        //发送消息时指定 header 延迟时间
+        rabbitTemplate.convertAndSend(RabbitMQConfig.DELAY_EXCHANGE, RabbitMQConfig.DELAY_USER_KEY, message,
+                message1 -> {
+                    //设置消息持久化
+                    message1.getMessageProperties().setDeliveryMode(MessageDeliveryMode.PERSISTENT);
+                    // 两种方式 均可
+                    //message.getMessageProperties().setHeader("x-delay", "6000");
+                    message1.getMessageProperties().setDelay(delayTime);
+                    return message1;
+                }, correlationData);
+    }
+}
