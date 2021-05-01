e0de40c0da5c3e65f8a59ec33efc3eb219cf1f37
guoguang0536@gmail.com <guoguang0536@gmail.com>
2021-05-01 11:56:43 +0800

    :art: 优化代码结构

--- madao-web-gateway/src/main/java/com/madao/gateway/GateWayApplication.java
+++ madao-web-gateway/src/main/java/com/madao/gateway/GateWayApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.annotation.PostConstruct;
@@ -16,25 +14,22 @@ import javax.annotation.PostConstruct;
  * 系统采用Oauth2的客户端模式和授权码模式两种模式
  **/
 
-@ComponentScan(basePackages = {"com.madao"}, excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE)
-})
 @EnableFeignClients(basePackages = "com.madao.api")
 @SpringBootApplication
 public class GateWayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GateWayApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
 
-    @PostConstruct
-    void started() {
-        DateUtil.setDefaultZone();
-    }
+	@PostConstruct
+	void started() {
+		DateUtil.setDefaultZone();
+	}
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
 
