a3ecb89a020dcdbd3255c4aa6052f369e754185d
guoguang0536@gmail.com <guoguang0536@gmail.com>
2021-07-04 23:58:21 +0800

    :sparkles: 新增聚合注解

---
+++ madao-common/src/main/java/com/madao/annotation/EnableSpringCloudComponent.java
@@ -0,0 +1,21 @@
+package com.madao.annotation;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@EnableEurekaClient
+@SpringBootApplication
+@EntityScan("com.madao.model.pojo")
+@EnableFeignClients("com.madao.api")
+@ComponentScan(basePackages = {"com.madao"})
+public @interface EnableSpringCloudComponent {
+
+}
