6add37101b706c100469119d8ff1bc5be22918fe
guoguang0536@gmail.com <guoguang0536@gmail.com>
2021-12-01 22:36:00 +0800

    :sparkles: 定时任务线程池配置

--- madao-common/src/main/java/com/madao/annotation/EnableSpringCloudComponent.java
+++ madao-common/src/main/java/com/madao/annotation/EnableSpringCloudComponent.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.lang.annotation.*;
 
@@ -19,6 +20,7 @@ import java.lang.annotation.*;
 @EntityScan("com.madao.model.entity")
 @EnableFeignClients("com.madao.api")
 @ComponentScan(basePackages = {"com.madao"})
+@EnableScheduling
 @EnableCaching
 public @interface EnableSpringCloudComponent {
 
