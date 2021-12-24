f10e8f2ed05b9a5fc38726d4f94e03d4cfecc419
guoguang0536@gmail.com <guoguang0536@gmail.com>
2021-12-25 00:16:14 +0800

    :memo: 完善SQL注释

--- madao-common/src/main/java/com/madao/model/entity/base/OptLog.java
+++ madao-common/src/main/java/com/madao/model/entity/base/OptLog.java
@@ -2,11 +2,14 @@ package com.madao.model.entity.base;
 
 import com.madao.enums.OptLogType;
 import com.madao.model.BasePojo;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
  * @author GuoGuang
@@ -15,6 +18,8 @@ import java.util.Objects;
  * @website https://madaoo.com
  * @created 2019-09-29 7:37
  */
+@Getter
+@Setter
 @Entity
 @Table(name = "ba_opt_log",
         indexes = {
@@ -22,6 +27,8 @@ import java.util.Objects;
                 @Index(name = "opt_log_user_id", columnList = "userId"),
                 @Index(name = "opt_log_create_at", columnList = "createAt")
         })
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class OptLog extends BasePojo implements Serializable {
 
     @Id
@@ -30,124 +37,30 @@ public class OptLog extends BasePojo implements Serializable {
     @Column(name = "id", unique = true, nullable = false, updatable = false, length = 20)
     private String id;
 
-    @Column(length = 20)
+    @Column(columnDefinition="varchar(20) COMMENT '用户id'",nullable = false)
     private String userId;
 
     @Transient
     private String userName;
 
-    @Column(length = 20)
+    @Column(columnDefinition="varchar(20) COMMENT '客户端IP'",nullable = false)
     private String clientIp;
 
-    @Column(length = 1)
+    @Column(columnDefinition="tinyint(1) COMMENT '操作类型'",nullable = false)
     private OptLogType type;
 
-    @Column(length = 100)
+    @Column(columnDefinition="varchar(100) COMMENT '方法'",nullable = false)
     private String method;
 
-    @Column(length = 500)
+    @Column(columnDefinition="varchar(500) COMMENT '参数'")
     private String params;
 
-    @Column(length = 500)
+    @Column(columnDefinition="varchar(500) COMMENT '异常信息'")
     private String exceptionDetail;
 
-    @Column(length = 50)
+    @Column(columnDefinition="varchar(50) COMMENT '浏览器'")
     private String browser;
 
-    @Column(length = 50)
+    @Column(columnDefinition="varchar(50) COMMENT '系统信息'")
     private String osInfo;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getClientIp() {
-		return clientIp;
-	}
-
-	public void setClientIp(String clientIp) {
-		this.clientIp = clientIp;
-	}
-
-	public OptLogType getType() {
-		return type;
-	}
-
-	public void setType(OptLogType type) {
-		this.type = type;
-	}
-
-	public String getMethod() {
-		return method;
-	}
-
-	public void setMethod(String method) {
-		this.method = method;
-	}
-
-	public String getParams() {
-		return params;
-	}
-
-	public void setParams(String params) {
-		this.params = params;
-	}
-
-	public String getExceptionDetail() {
-		return exceptionDetail;
-	}
-
-	public void setExceptionDetail(String exceptionDetail) {
-		this.exceptionDetail = exceptionDetail;
-	}
-
-	public String getBrowser() {
-		return browser;
-	}
-
-	public void setBrowser(String browser) {
-		this.browser = browser;
-	}
-
-	public String getOsInfo() {
-		return osInfo;
-	}
-
-	public void setOsInfo(String osInfo) {
-		this.osInfo = osInfo;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof OptLog)) return false;
-		if (!super.equals(o)) return false;
-		OptLog optLog = (OptLog) o;
-		return Objects.equals(id, optLog.id) && Objects.equals(userId, optLog.userId) && Objects.equals(userName, optLog.userName) && Objects.equals(clientIp, optLog.clientIp) && type == optLog.type && Objects.equals(method, optLog.method) && Objects.equals(params, optLog.params) && Objects.equals(exceptionDetail, optLog.exceptionDetail) && Objects.equals(browser, optLog.browser) && Objects.equals(osInfo, optLog.osInfo);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(super.hashCode(), id, userId, userName, clientIp, type, method, params, exceptionDetail, browser, osInfo);
-	}
 }
