98231f8a35ad4600c078faadb4e428e282cf1257
guoguang0536@gmail.com <guoguang0536@gmail.com>
2022-04-03 10:26:19 +0800

    :wrench: 添加用户ForkJoinPool

--- madao-common/src/main/java/com/madao/config/BusinessThreadPool.java
+++ madao-common/src/main/java/com/madao/config/BusinessThreadPool.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinWorkerThread;
 
 @Configuration
 public class BusinessThreadPool {
@@ -14,9 +16,21 @@ public class BusinessThreadPool {
 	 * 普通线程池
 	 */
 	@Bean
-	public ExecutorService fixedThreadPool(){
-		return Executors.newFixedThreadPool(10,new ThreadFactoryBuilder()
+	public ExecutorService fixedThreadPool() {
+		return Executors.newFixedThreadPool(10, new ThreadFactoryBuilder()
 				.setNameFormat("common-fixed-pool-%d").build());
+	}
+
+	/**
+	 * 用户服务ForkJoinPool
+	 */
+	@Bean
+	public static ExecutorService userWorkStealingPool() {
+		return new ForkJoinPool(Runtime.getRuntime().availableProcessors(), pool -> {
+			final ForkJoinWorkerThread worker = ForkJoinPool.defaultForkJoinWorkerThreadFactory.newThread(pool);
+			worker.setName("USER-ForkJoinPool-1-worker- " + worker.getPoolIndex());
+			return worker;
+		}, null, true);
 	}
 
 	/**
