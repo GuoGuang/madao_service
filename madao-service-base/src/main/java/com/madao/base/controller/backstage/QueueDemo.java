76342185534bb39fa4e9933afa5cd8b4eab5e2eb
guoguang0536@gmail.com <guoguang0536@gmail.com>
2022-11-28 14:54:18 +0800

    :sparkles: 添加业务场景

--- madao-service-base/src/main/java/com/madao/base/controller/backstage/QueueDemo.java
+++ madao-service-base/src/main/java/com/madao/base/controller/backstage/QueueDemo.java
@@ -6,6 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 请求排队、取消排队、查看排队状态
+ * @author GuoGuang
+ * @公众号 码道人生
+ * @gitHub https://github.com/GuoGuang
+ * @website https://madaoo.com
+ * @created 2022年11月28日14:35:46
+ */
 @RestController
 @AllArgsConstructor
 public class QueueDemo {
