6abb0f027d89374900fb9f0b01fbf43c072c3296
lgg <linguoguang@inca.com.cn>
2023-05-13 13:06:08 +0800

    :wrench: 代码优化

--- madao-common/src/main/java/com/madao/annotation/EnableSpringCloudComponent.java
+++ madao-common/src/main/java/com/madao/annotation/EnableSpringCloudComponent.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,7 +13,6 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@EnableEurekaClient
 @SpringBootApplication
 @EnableDiscoveryClient
 @EntityScan("com.madao.model.entity")
--- madao-service-article/src/main/java/com/madao/article/ArticleApplication.java
+++ madao-service-article/src/main/java/com/madao/article/ArticleApplication.java
@@ -2,14 +2,13 @@ package com.madao.article;
 
 import com.madao.annotation.EnableSpringCloudComponent;
 import com.madao.utils.DateUtil;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.PostConstruct;
 
 /**
  * SpringBoot主配置类只会扫描自己所在的包及其子包下面,需要通过@ComponentScan处理
