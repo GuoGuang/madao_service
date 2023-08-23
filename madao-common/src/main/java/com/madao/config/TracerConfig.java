c397cc0d6cbaa35620d1f727f4d31bd442cf5166
guoguang0536 <guoguang0536@gmail.com>
2023-08-23 14:42:34 +0800

    :zap: micrometer追踪配置调整，org.springframework.cloud.sleuth包改为io.micrometer.tracing

--- madao-common/src/main/java/com/madao/config/TracerConfig.java
+++ madao-common/src/main/java/com/madao/config/TracerConfig.java
@@ -1,16 +1,16 @@
 package com.madao.config;
 
+
 import brave.baggage.*;
 import ch.qos.logback.classic.ClassicConstants;
 import cn.hutool.json.JSONObject;
 import com.madao.utils.security.JWTAuthentication;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.cloud.sleuth.Span;
-import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
@@ -39,36 +39,31 @@ public class TracerConfig {
 	 */
 	@Bean
 	WebFilter tracerWebFilter(Optional<Tracer> tracer) {
-		return (exchange, chain) -> {
-			exchange.getResponse().beforeCommit(() -> {
-				Optional.ofNullable(exchange.<String>getAttribute(ServerWebExchange.LOG_ID_ATTRIBUTE)).ifPresent(logId -> exchange.getResponse().getHeaders().add("logId", logId));
-				return Mono.empty();
-			});
-			return chain.filter(exchange)
-					// 执行完请求后将相关traceId、spanID返回前端，方便排查
-					.doOnSubscribe(subscription -> tracer.ifPresent(t -> {
-						Optional.ofNullable(t.currentSpan()).map(Span::context).ifPresent(context -> exchange.getResponse().beforeCommit(() -> {
-							Optional.ofNullable(context.traceId()).ifPresent(traceId -> exchange.getResponse().getHeaders().add("traceId", traceId));
-							Optional.ofNullable(context.spanId()).ifPresent(spanId -> exchange.getResponse().getHeaders().add("spanId", spanId));
-							Optional.ofNullable(context.parentId()).ifPresent(parentId -> exchange.getResponse().getHeaders().add("parentId", parentId));
-							return Mono.empty();
-						}));
-						// 执行请求前传播字段
-						t.createBaggage(ClassicConstants.REQUEST_REQUEST_URI, exchange.getRequest().getURI().getPath());
-						t.createBaggage(ClassicConstants.REQUEST_QUERY_STRING, exchange.getRequest().getURI().getQuery());
-						t.createBaggage(ClassicConstants.REQUEST_METHOD, exchange.getRequest().getMethodValue());
-						String instance = exchange.getRequest().getHeaders().getFirst("INSTANCE");
-						String authorization = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-
-						t.createBaggage("INSTANCE", instance);
-
-						//  日志中添加USER-ID，便于基于USER-ID分析用户行为
-						if (StringUtils.isNotEmpty(authorization) & StringUtils.startsWith(authorization, JWTAuthentication.BEARER)) {
-							JSONObject jsonObject = JWTAuthentication.parseJwtToClaimsAsJSONObject(authorization);
-							t.createBaggage("USER-ID", jsonObject.getStr("user_name"));
-						}
+		return (exchange, chain) -> chain.filter(exchange)
+				// 执行完请求后将相关traceId、spanID返回前端，方便排查
+				.doOnSubscribe(subscription -> tracer.ifPresent(t -> {
+					Optional.ofNullable(t.currentSpan()).map(Span::context).ifPresent(context -> exchange.getResponse().beforeCommit(() -> {
+						Optional.ofNullable(context.traceId()).ifPresent(traceId -> exchange.getResponse().getHeaders().add("traceId", traceId));
+						Optional.ofNullable(context.spanId()).ifPresent(spanId -> exchange.getResponse().getHeaders().add("spanId", spanId));
+						Optional.ofNullable(context.parentId()).ifPresent(parentId -> exchange.getResponse().getHeaders().add("parentId", parentId));
+						return Mono.empty();
 					}));
-		};
+					// 执行请求前传播字段：请求地址，请求人，请求方法，方便进一步排查问题
+					t.createBaggage(ClassicConstants.REQUEST_REQUEST_URI, exchange.getRequest().getURI().getPath());
+					t.createBaggage(ClassicConstants.REQUEST_QUERY_STRING, exchange.getRequest().getURI().getQuery());
+					t.createBaggage(ClassicConstants.REQUEST_METHOD, exchange.getRequest().getMethodValue());
+
+					// 获取请求中的实例ID作为传播的一部分，此实例标识用于前端来指定调用哪个IP，仅适用于开发环境联调使用
+					String instance = exchange.getRequest().getHeaders().getFirst("INSTANCE");
+					String authorization = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+					t.createBaggage("INSTANCE", instance);
+
+					//  日志中添加USER-ID，便于基于USER-ID分析用户行为
+					if (StringUtils.isNotEmpty(authorization) & StringUtils.startsWith(authorization, JWTAuthentication.BEARER)) {
+						JSONObject jsonObject = JWTAuthentication.parseJwtToClaimsAsJSONObject(authorization);
+						t.createBaggage("USER-ID", jsonObject.getStr("user_name"));
+					}
+				}));
 	}
 
 	/**
