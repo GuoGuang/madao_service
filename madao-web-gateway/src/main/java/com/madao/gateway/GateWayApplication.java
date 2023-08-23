11ad1f2eae3d2ea98daf5bfc596955e167d683c6
guoguang0536 <guoguang0536@gmail.com>
2023-08-23 14:35:57 +0800

    :zap: 网关层重大调整，标准适配gateway，集成到micrometer

--- madao-web-gateway/src/main/java/com/madao/gateway/GateWayApplication.java
+++ madao-web-gateway/src/main/java/com/madao/gateway/GateWayApplication.java
@@ -1,15 +1,23 @@
 package com.madao.gateway;
 
+import com.madao.annotation.EnableSpringCloudComponent;
 import com.madao.gateway.config.CustomLoadBalancerConfiguration;
 import com.madao.utils.DateUtil;
+import com.netflix.discovery.AbstractDiscoveryClientOptionalArgs;
+import com.netflix.discovery.shared.transport.jersey.TransportClientFactories;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.http.EurekaClientHttpRequestFactorySupplier;
+import org.springframework.cloud.netflix.eureka.http.RestTemplateDiscoveryClientOptionalArgs;
+import org.springframework.cloud.netflix.eureka.http.RestTemplateTransportClientFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import reactor.core.publisher.Hooks;
 
 /**
  * gateway服务网关
@@ -21,13 +29,31 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * @website https://madaoo.com
  * @created 2019-09-29 7:37
  */
-@EnableFeignClients(basePackages = "com.madao.api")
-@SpringBootApplication
+@EnableSpringCloudComponent
 @LoadBalancerClients(defaultConfiguration = CustomLoadBalancerConfiguration.class)
 public class GateWayApplication {
 
 	public static void main(String[] args) {
+		// 在响应式编程上下文中启用自动上下文传播
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(GateWayApplication.class, args);
+	}
+
+
+	/**
+	 * 因为此bug：https://github.com/spring-cloud/spring-cloud-netflix/issues/4185，不得不在此处添加两个Bean
+	 */
+	@Bean
+	@ConditionalOnClass(name = { "org.springframework.web.client.RestTemplate", "org.glassfish.jersey.client.JerseyClient" })
+	@ConditionalOnMissingBean(value = { AbstractDiscoveryClientOptionalArgs.class }, search = SearchStrategy.CURRENT)
+	public RestTemplateDiscoveryClientOptionalArgs restTemplateDiscoveryClientOptionalArgs(EurekaClientHttpRequestFactorySupplier eurekaClientHttpRequestFactorySupplier) {
+		return new RestTemplateDiscoveryClientOptionalArgs(eurekaClientHttpRequestFactorySupplier);
+	}
+	@Bean
+	@ConditionalOnClass(name = { "org.springframework.web.client.RestTemplate", "org.glassfish.jersey.client.JerseyClient" })
+	@ConditionalOnMissingBean(value = { TransportClientFactories.class }, search = SearchStrategy.CURRENT)
+	public RestTemplateTransportClientFactories restTemplateTransportClientFactories(RestTemplateDiscoveryClientOptionalArgs optionalArgs) {
+		return new RestTemplateTransportClientFactories(optionalArgs);
 	}
 
 	@PostConstruct
