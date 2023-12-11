0a6a77c4808e8382da5ae64c1dc75ea5d23b12a8
guoguang0536@gmail.com <guoguang0536@gmail.com>
2023-12-11 22:00:26 +0800

    :arrow_up: springboot升级到2.3.0、springcloud升级到2023.0.0

--- madao-common/src/main/java/com/madao/config/TracerConfig.java
+++ madao-common/src/main/java/com/madao/config/TracerConfig.java
@@ -28,67 +28,67 @@ import java.util.Optional;
 @Configuration(proxyBeanMethods = false)
 public class TracerConfig {
 
-	BaggageField requestRequestUri = BaggageField.create(ClassicConstants.REQUEST_REQUEST_URI);
-	BaggageField requestQueryString = BaggageField.create(ClassicConstants.REQUEST_QUERY_STRING);
-	BaggageField requestMethod = BaggageField.create(ClassicConstants.REQUEST_METHOD);
-	BaggageField userId = BaggageField.create("USER-ID");
+    BaggageField requestRequestUri = BaggageField.create(ClassicConstants.REQUEST_REQUEST_URI);
+    BaggageField requestQueryString = BaggageField.create(ClassicConstants.REQUEST_QUERY_STRING);
+    BaggageField requestMethod = BaggageField.create(ClassicConstants.REQUEST_METHOD);
+    BaggageField userId = BaggageField.create("USER-ID");
 
 
-	/**
-	 * 响应前响应后过滤器
-	 */
-	@Bean
-	WebFilter tracerWebFilter(Optional<Tracer> tracer) {
-		return (exchange, chain) -> chain.filter(exchange)
-				// 执行完请求后将相关traceId、spanID返回前端，方便排查
-				.doOnSubscribe(subscription -> tracer.ifPresent(t -> {
-					Optional.ofNullable(t.currentSpan()).map(Span::context).ifPresent(context -> exchange.getResponse().beforeCommit(() -> {
-						Optional.ofNullable(context.traceId()).ifPresent(traceId -> exchange.getResponse().getHeaders().add("traceId", traceId));
-						Optional.ofNullable(context.spanId()).ifPresent(spanId -> exchange.getResponse().getHeaders().add("spanId", spanId));
-						Optional.ofNullable(context.parentId()).ifPresent(parentId -> exchange.getResponse().getHeaders().add("parentId", parentId));
-						return Mono.empty();
-					}));
-					// 执行请求前传播字段：请求地址，请求人，请求方法，方便进一步排查问题
-					t.createBaggage(ClassicConstants.REQUEST_REQUEST_URI, exchange.getRequest().getURI().getPath());
-					t.createBaggage(ClassicConstants.REQUEST_QUERY_STRING, exchange.getRequest().getURI().getQuery());
-					t.createBaggage(ClassicConstants.REQUEST_METHOD, exchange.getRequest().getMethodValue());
+    /**
+     * 响应前响应后过滤器
+     */
+    @Bean
+    WebFilter tracerWebFilter(Optional<Tracer> tracer) {
+        return (exchange, chain) -> chain.filter(exchange)
+                // 执行完请求后将相关traceId、spanID返回前端，方便排查
+                .doOnSubscribe(subscription -> tracer.ifPresent(t -> {
+                    Optional.ofNullable(t.currentSpan()).map(Span::context).ifPresent(context -> exchange.getResponse().beforeCommit(() -> {
+                        Optional.ofNullable(context.traceId()).ifPresent(traceId -> exchange.getResponse().getHeaders().add("traceId", traceId));
+                        Optional.ofNullable(context.spanId()).ifPresent(spanId -> exchange.getResponse().getHeaders().add("spanId", spanId));
+                        Optional.ofNullable(context.parentId()).ifPresent(parentId -> exchange.getResponse().getHeaders().add("parentId", parentId));
+                        return Mono.empty();
+                    }));
+                    // 执行请求前传播字段：请求地址，请求人，请求方法，方便进一步排查问题
+                    t.createBaggageInScope(ClassicConstants.REQUEST_REQUEST_URI, exchange.getRequest().getURI().getPath()).close();
+                    t.createBaggageInScope(ClassicConstants.REQUEST_QUERY_STRING, exchange.getRequest().getURI().getQuery()).close();
+                    t.createBaggageInScope(ClassicConstants.REQUEST_METHOD, exchange.getRequest().getMethod().name()).close();
 
-					// 获取请求中的实例ID作为传播的一部分，此实例标识用于前端来指定调用哪个IP，仅适用于开发环境联调使用
-					String instance = exchange.getRequest().getHeaders().getFirst("INSTANCE");
-					String authorization = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-					t.createBaggage("INSTANCE", instance);
+                    // 获取请求中的实例ID作为传播的一部分，此实例标识用于前端来指定调用哪个IP，仅适用于开发环境联调使用
+                    String instance = exchange.getRequest().getHeaders().getFirst("INSTANCE");
+                    String authorization = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+                    t.createBaggageInScope("INSTANCE", instance).close();
 
-					//  日志中添加USER-ID，便于基于USER-ID分析用户行为
-					if (StringUtils.isNotEmpty(authorization) & StringUtils.startsWith(authorization, JWTAuthentication.BEARER)) {
-						JSONObject jsonObject = JWTAuthentication.parseJwtToClaimsAsJSONObject(authorization);
-						t.createBaggage("USER-ID", jsonObject.getStr("user_name"));
-					}
-				}));
-	}
+                    //  日志中添加USER-ID，便于基于USER-ID分析用户行为
+                    if (StringUtils.isNotEmpty(authorization) & StringUtils.startsWith(authorization, JWTAuthentication.BEARER)) {
+                        JSONObject jsonObject = JWTAuthentication.parseJwtToClaimsAsJSONObject(authorization);
+                        t.createBaggageInScope("USER-ID", jsonObject.getStr("user_name")).close();
+                    }
+                }));
+    }
 
-	/**
-	 * 传播字段定制器，SingleBaggageField.remote请求时携带请求头
-	 */
-	@Bean
-	BaggagePropagationCustomizer baggagePropagationCustomizer() {
-		return builder -> builder
-				.add(BaggagePropagationConfig.SingleBaggageField.remote(requestRequestUri))
-				.add(BaggagePropagationConfig.SingleBaggageField.remote(requestQueryString))
-				.add(BaggagePropagationConfig.SingleBaggageField.remote(userId))
-				.add(BaggagePropagationConfig.SingleBaggageField.remote(requestMethod));
-	}
+    /**
+     * 传播字段定制器，SingleBaggageField.remote请求时携带请求头
+     */
+    @Bean
+    BaggagePropagationCustomizer baggagePropagationCustomizer() {
+        return builder -> builder
+                .add(BaggagePropagationConfig.SingleBaggageField.remote(requestRequestUri))
+                .add(BaggagePropagationConfig.SingleBaggageField.remote(requestQueryString))
+                .add(BaggagePropagationConfig.SingleBaggageField.remote(userId))
+                .add(BaggagePropagationConfig.SingleBaggageField.remote(requestMethod));
+    }
 
-	/**
-	 * 相关范围定制器
-	 */
-	@Bean
-	CorrelationScopeCustomizer correlationScopeCustomizer() {
-		return builder -> builder
-				.add(CorrelationScopeConfig.SingleCorrelationField.create(BaggageFields.PARENT_ID))
-				.add(CorrelationScopeConfig.SingleCorrelationField.create(BaggageFields.SAMPLED))
-				.add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(requestRequestUri).flushOnUpdate().build())
-				.add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(requestQueryString).flushOnUpdate().build())
-				.add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(requestMethod).flushOnUpdate().build())
-				.add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(userId).flushOnUpdate().build());
-	}
+    /**
+     * 相关范围定制器
+     */
+    @Bean
+    CorrelationScopeCustomizer correlationScopeCustomizer() {
+        return builder -> builder
+                .add(CorrelationScopeConfig.SingleCorrelationField.create(BaggageFields.PARENT_ID))
+                .add(CorrelationScopeConfig.SingleCorrelationField.create(BaggageFields.SAMPLED))
+                .add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(requestRequestUri).flushOnUpdate().build())
+                .add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(requestQueryString).flushOnUpdate().build())
+                .add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(requestMethod).flushOnUpdate().build())
+                .add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(userId).flushOnUpdate().build());
+    }
 }
--- madao-web-gateway/src/main/java/com/madao/gateway/config/Swagger3Config.java
+++ madao-web-gateway/src/main/java/com/madao/gateway/config/Swagger3Config.java
@@ -1,27 +1,20 @@
 package com.madao.gateway.config;
 
-import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.SwaggerUiConfigParameters;
-import org.springdoc.core.SwaggerUiOAuthProperties;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 @Profile("dev")
-@Import({ org.springdoc.core.SpringDocConfigProperties.class,
-		org.springdoc.webmvc.core.MultipleOpenApiSupportConfiguration.class,
-		org.springdoc.core.SpringDocConfiguration.class, org.springdoc.webmvc.core.SpringDocWebMvcConfiguration.class,
-		SwaggerUiConfigParameters.class, SwaggerUiOAuthProperties.class,
-		org.springdoc.core.SwaggerUiConfigProperties.class, org.springdoc.core.SwaggerUiOAuthProperties.class,
-		org.springdoc.webmvc.ui.SwaggerConfig.class,
-		org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class })
 public class Swagger3Config {
-
-
 
 	@Bean
 	@Lazy(false)
